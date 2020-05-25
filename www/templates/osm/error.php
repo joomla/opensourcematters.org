916d4d391accfd76cacbcdcc529d364d46f48791
brian teeman <brian@teeman.net>
2020-05-25 15:42:08 +0100

    remove back to top as the template has no language strings

--- www/templates/osm/error.php
+++ www/templates/osm/error.php
@@ -192,7 +192,6 @@ else
 			<hr />
 			<?php echo $doc->getBuffer('modules', 'footer', array('style' => 'none')); ?>
 
-			<p class="pull-right"><a href="#top" id="back-top"><?php echo JText::_('TPL_PROTOSTAR_BACKTOTOP'); ?></a></p>
 			<p>&copy; <?php echo $sitename; ?> <?php echo date('Y');?></p>
 		</div>
 	</div>
